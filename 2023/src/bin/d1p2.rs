ffa838684cf9b1c98687640adebc2d9146a0c715
westy12dan <westy12dan@gmail.com>
2023-12-01 16:41:56 +0800

    imp

--- 2023/src/bin/d1p2.rs
+++ 2023/src/bin/d1p2.rs
@@ -1,68 +1,71 @@
-
 fn main() {
     let input = std::fs::read_to_string("src/input/d1p1.txt").unwrap();
-    
-    let total = input.lines().map(|line| {
-        let mut first = '0';
-        let mut last = '0';
 
-        // First
-        for (i, c) in line.chars().enumerate() {
-            if c.is_numeric() {
-                first = c;
-                break;
-            } else if c.is_alphabetic() {
-                let num = is_word_num(i, line, true);
-                if num.is_some() {
-                    first = num.unwrap();
+    let total = input
+        .lines()
+        .map(|line| {
+            let mut first = 0;
+            let mut last = 0;
+
+            // First
+            for (i, c) in line.chars().enumerate() {
+                if c.is_numeric() {
+                    first = c.to_digit(10).unwrap();
                     break;
+                } else if c.is_alphabetic() {
+                    let num = is_word_num(i, line, true);
+                    if let Some(n) = num {
+                        first = n;
+                        break;
+                    }
                 }
             }
-        }
 
-        // Last
-        for (i, c) in line.chars().rev().enumerate() {
-            if c.is_numeric() {
-                let len = line.len();
-                last = line[len-i-1..len-i].parse::<char>().unwrap();
-                break;
-            } else if c.is_alphabetic() {
-                let num = is_word_num(line.len()-i, line, false);
-                if num.is_some() {
-                    last = num.unwrap();
+            // Last
+            for (i, c) in line.chars().rev().enumerate() {
+                if c.is_numeric() {
+                    last = c.to_digit(10).unwrap();
                     break;
+                } else if c.is_alphabetic() {
+                    let num = is_word_num(line.len() - i, line, false);
+                    if let Some(n) = num {
+                        last = n;
+                        break;
+                    }
                 }
             }
-        }
 
-        return format!("{}{}", first, last).parse::<i32>().unwrap();
-    }).fold(0, |acc, num| {
-        println!("{}", num);
-        acc + num
-    });
+            return format!("{}{}", first, last).parse::<u32>().unwrap();
+        })
+        .fold(0, |acc, num| {
+            println!("{}", num);
+            acc + num
+        });
 
     println!("Total: {}", total);
 }
 
-fn is_word_num(i: usize, line: &str, forward: bool) -> Option<char> {
-    let nums_word = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    let nums = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+fn is_word_num(i: usize, line: &str, forward: bool) -> Option<u32> {
+    let nums_word = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let nums = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     if forward {
         for (j, word) in nums_word.iter().enumerate() {
-            let is_oob = i+word.len() > line.len();
-            if !is_oob && line[i..i+word.len()].to_lowercase() == *word {
-                return Some(nums[j].parse::<char>().unwrap());
+            let is_oob = i + word.len() > line.len();
+            if !is_oob && line[i..i + word.len()].to_lowercase() == *word {
+                return Some(nums[j]);
             }
         }
     } else {
         for (j, word) in nums_word.iter().enumerate() {
             let is_oob = i < word.len();
-            if !is_oob && line[i-word.len()..i].to_lowercase() == *word {
-                return Some(nums[j].parse::<char>().unwrap());
+            if !is_oob && line[i - word.len()..i].to_lowercase() == *word {
+                return Some(nums[j]);
             }
         }
     }
 
-    return None
+    return None;
 }
