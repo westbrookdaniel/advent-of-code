2cf71cce344f5bd0c7bb567aecc4327cf1925c55
westy12dan <westy12dan@gmail.com>
2023-12-02 13:44:23 +0800

    first try again

---
+++ 2023/src/bin/d2p2.rs
@@ -0,0 +1,57 @@
+#[derive(Debug)]
+struct Game {
+    id: i32,
+    subsets: Vec<Vec<(i32, String)>>,
+}
+
+impl From<&str> for Game {
+    fn from(s: &str) -> Self {
+        let mut parts = s.split(": ");
+        let id = parts.next().unwrap()[5..].parse::<i32>().unwrap();
+        let subsets = parts
+            .next()
+            .unwrap()
+            .split("; ")
+            .map(|subset| {
+                subset
+                    .split(", ")
+                    .map(|item| {
+                        let mut item = item.split(' ');
+                        let num = item.next().unwrap().parse::<i32>().unwrap();
+                        let word = item.next().unwrap().to_string();
+                        (num, word)
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        Game { id, subsets }
+    }
+}
+
+fn main() {
+    let input = std::fs::read_to_string("src/input/d2p1.txt").unwrap();
+
+    let total: i32 = input
+        .lines()
+        .map(|line| Game::from(line))
+        .map(|game| {
+            let mut red = 0;
+            let mut blue = 0;
+            let mut green = 0;
+
+            game.subsets.iter().for_each(|subset| {
+                subset.iter().for_each(|(num, color)| match color.as_str() {
+                    "red" => red = red.max(*num),
+                    "blue" => blue = blue.max(*num),
+                    "green" => green = green.max(*num),
+                    _ => (),
+                })
+            });
+
+            red * blue * green
+        })
+        .sum();
+
+    println!("Total: {}", total);
+}
