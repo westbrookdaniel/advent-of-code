37f91d9009d0479546801cb36015c46f2541b824
westy12dan <westy12dan@gmail.com>
2023-12-17 18:33:42 +0800

    eh

--- 2023/src/bin/d17p1.rs
+++ 2023/src/bin/d17p1.rs
@@ -1,4 +1,4 @@
-use pathfinding::directed::dijkstra::dijkstra;
+use pathfinding::{directed::dijkstra::dijkstra, matrix::Matrix};
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Pos(i32, i32, Dir, i32);
@@ -11,86 +11,66 @@ enum Dir {
     Right,
 }
 
-fn get_char(grid: &Vec<Vec<char>>, pos: (i32, i32)) -> Option<char> {
-    let c = grid.get(pos.1 as usize)?.get(pos.0 as usize)?;
-    Some(*c)
-}
-
 impl Pos {
-    fn loss(&self, grid: &Vec<Vec<char>>) -> u32 {
-        let char = get_char(grid, (self.0, self.1));
+    fn loss(&self, grid: &Matrix<u32>) -> u32 {
+        let dig = grid.get((self.0 as usize, self.1 as usize));
 
-        if let Some(char) = char {
-            char.to_digit(10).unwrap() as u32
+        if let Some(dig) = dig {
+            *dig
         } else {
-            10
+            1000
         }
     }
 
-    fn successors(&self, grid: &Vec<Vec<char>>) -> Vec<(Pos, u32)> {
-        let &Pos(x, y, dir, dist) = self;
+    fn weight(&self, grid: &Matrix<u32>) -> u32 {
+        let loss = self.loss(&grid);
+        loss
+    }
 
-        if dist == 3 {
-            let possible_dirs = match dir {
-                Dir::Up => vec![Dir::Left, Dir::Right],
-                Dir::Down => vec![Dir::Left, Dir::Right],
-                Dir::Left => vec![Dir::Up, Dir::Down],
-                Dir::Right => vec![Dir::Up, Dir::Down],
-            };
+    fn successors(&self, grid: &Matrix<u32>) -> Vec<(Pos, u32)> {
+        let &Pos(x, y, dir, mut dist) = self;
 
-            let dist = 0;
-            return possible_dirs
-                .into_iter()
-                .map(|dir| match dir {
-                    Dir::Up => Pos(x, y - 1, dir, dist + 1),
-                    Dir::Down => Pos(x, y + 1, dir, dist + 1),
-                    Dir::Left => Pos(x - 1, y, dir, dist + 1),
-                    Dir::Right => Pos(x + 1, y, dir, dist + 1),
-                })
-                .map(|p| {
-                    let l = p.loss(&grid);
-                    (p, l)
-                })
-                .collect();
-        } else {
-            let possible_dirs = match dir {
-                Dir::Up => vec![Dir::Up, Dir::Left, Dir::Right],
-                Dir::Down => vec![Dir::Down, Dir::Left, Dir::Right],
-                Dir::Left => vec![Dir::Left, Dir::Up, Dir::Down],
-                Dir::Right => vec![Dir::Right, Dir::Up, Dir::Down],
-            };
-
-            return possible_dirs
-                .into_iter()
-                .map(|dir| match dir {
-                    Dir::Up => Pos(x, y - 1, dir, dist + 1),
-                    Dir::Down => Pos(x, y + 1, dir, dist + 1),
-                    Dir::Left => Pos(x - 1, y, dir, dist + 1),
-                    Dir::Right => Pos(x + 1, y, dir, dist + 1),
-                })
-                .map(|p| {
-                    let l = p.loss(&grid);
-                    (p, l)
-                })
-                .collect();
+        let mut possible_dirs = match dir {
+            Dir::Up => vec![Dir::Left, Dir::Right],
+            Dir::Down => vec![Dir::Left, Dir::Right],
+            Dir::Left => vec![Dir::Up, Dir::Down],
+            Dir::Right => vec![Dir::Up, Dir::Down],
         };
+
+        if dist < 3 {
+            possible_dirs.push(dir);
+        } else {
+            dist = 0;
+        }
+
+        return possible_dirs
+            .into_iter()
+            .map(|dir| match dir {
+                Dir::Up => Pos(x, y - 1, dir, dist + 1),
+                Dir::Down => Pos(x, y + 1, dir, dist + 1),
+                Dir::Left => Pos(x - 1, y, dir, dist + 1),
+                Dir::Right => Pos(x + 1, y, dir, dist + 1),
+            })
+            .map(|p| {
+                let l = p.weight(&grid);
+                (p, l)
+            })
+            .collect();
     }
 }
-
-// assert_eq!(result.expect("no path found").1, 4);
 
 fn main() {
     let input = std::fs::read_to_string("src/input/d17p1.txt").unwrap();
 
-    let grid = input
-        .trim()
-        .lines()
-        .map(|line| line.trim().chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
+    let grid = Matrix::from_rows(input.trim().lines().map(|line| {
+        line.trim()
+            .chars()
+            .map(|c| c.to_digit(10).unwrap())
+            .collect::<Vec<u32>>()
+    }))
+    .unwrap();
 
-    let goal = (grid[0].len() as i32 - 1, grid.len() as i32 - 1);
-
-    let start_weight = Pos(0, 0, Dir::Right, 0).loss(&grid);
+    let goal = (grid.columns as i32 - 1, grid.rows as i32 - 1);
 
     let result = dijkstra(
         &Pos(0, 0, Dir::Right, 0),
@@ -99,26 +79,30 @@ fn main() {
     )
     .unwrap();
 
+    print_journey(&result.0, &grid);
+
+    let init = result.0[0].loss(&grid);
     let l = result.0.iter().fold(0, |acc, p| acc + p.loss(&grid));
 
-    print_grid(&grid, &result);
     println!();
-    println!("Result: {}", l - start_weight);
+    println!("Result: {}", l - init);
 }
 
-fn print_grid(grid: &Vec<Vec<char>>, result: &(Vec<Pos>, u32)) {
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
+fn print_journey(pos: &Vec<Pos>, grid: &Matrix<u32>) {
+    for y in 0..grid.rows {
+        for x in 0..grid.columns {
             let mut found = false;
-            for p in &result.0 {
+            for p in pos {
                 if p.0 == x as i32 && p.1 == y as i32 {
-                    print!("{} ", p.loss(&grid));
                     found = true;
                     break;
                 }
             }
-            if !found {
-                print!("  ");
+
+            if found {
+                print!("{}", grid.get((x, y)).unwrap());
+            } else {
+                print!(" ");
             }
         }
         println!();
