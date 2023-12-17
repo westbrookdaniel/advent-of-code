079a382758ccc9cce15199f2bbd385c458460d98
westy12dan <westy12dan@gmail.com>
2023-12-17 21:00:21 +0800

    ehhh

--- 2023/src/bin/d17p2.rs
+++ 2023/src/bin/d17p2.rs
@@ -20,14 +20,18 @@ impl Pos {
     fn successors(&self, grid: &Matrix<u32>) -> Vec<(Pos, u32)> {
         let &Pos(x, y, curr_dir, mut dist) = self;
 
-        let mut possible_dirs = match curr_dir {
-            Dir::Up => vec![Dir::Left, Dir::Right],
-            Dir::Down => vec![Dir::Left, Dir::Right],
-            Dir::Left => vec![Dir::Up, Dir::Down],
-            Dir::Right => vec![Dir::Up, Dir::Down],
-        };
+        let mut possible_dirs = vec![];
 
-        if dist < 3 {
+        if dist > 3 {
+            possible_dirs = match curr_dir {
+                Dir::Up => vec![Dir::Left, Dir::Right],
+                Dir::Down => vec![Dir::Left, Dir::Right],
+                Dir::Left => vec![Dir::Up, Dir::Down],
+                Dir::Right => vec![Dir::Up, Dir::Down],
+            };
+        }
+
+        if dist < 10 {
             possible_dirs.push(curr_dir);
         } else {
             dist = 0;
@@ -74,17 +78,40 @@ fn main() {
     }))
     .unwrap();
 
-    let goal = (grid.columns as i32 - 1, grid.rows as i32 - 1);
+    let goal = (grid.rows as i32 - 1, grid.columns as i32 - 1);
 
     let result = dijkstra(
         &Pos(0, 0, Dir::Down, 0),
         |p| p.successors(&grid),
-        |p| p.0 == goal.0 && p.1 == goal.1,
+        |p| p.0 == goal.0 && p.1 == goal.1 && p.3 > 3,
     )
     .unwrap();
+
+    print_journey(&result.0, &grid);
 
     let init = result.0[0].loss(&grid);
     let l = result.0.iter().fold(0, |acc, p| acc + p.loss(&grid));
 
     println!("{}", l - init);
+}
+
+fn print_journey(pos: &Vec<Pos>, grid: &Matrix<u32>) {
+    for x in 0..grid.rows {
+        for y in 0..grid.columns {
+            let mut found = false;
+            for p in pos {
+                if p.0 == x as i32 && p.1 == y as i32 {
+                    found = true;
+                    break;
+                }
+            }
+
+            if found {
+                print!("{}", grid.get((x, y)).unwrap());
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
 }
