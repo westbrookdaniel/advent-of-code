3d58169c884299d1d1b11dfb4747f6bd0947b3c9
westy12dan <westy12dan@gmail.com>
2023-12-17 16:09:17 +0800

    astar

--- 2023/src/bin/d17p1.rs
+++ 2023/src/bin/d17p1.rs
@@ -1,3 +1,82 @@
+use pathfinding::prelude::astar;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(i32, i32, Dir, i32);
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Dir {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn get_char(grid: &Vec<Vec<char>>, pos: (i32, i32)) -> Option<char> {
+    let c = grid.get(pos.1 as usize)?.get(pos.0 as usize)?;
+    Some(*c)
+}
+
+impl Pos {
+    fn distance(&self, other: (i32, i32)) -> u32 {
+        (self.0.abs_diff(other.0) + self.1.abs_diff(other.1)) as u32
+    }
+
+    fn loss(&self, grid: &Vec<Vec<char>>) -> u32 {
+        let char = get_char(grid, (self.0, self.1));
+
+        if let Some(char) = char {
+            char.to_digit(10).unwrap() as u32
+        } else {
+            10
+        }
+    }
+
+    fn successors(&self) -> Vec<(Pos, u32)> {
+        let &Pos(x, y, dir, dist) = self;
+
+        if dist == 3 {
+            let possible_dirs = match dir {
+                Dir::Up => vec![Dir::Left, Dir::Right],
+                Dir::Down => vec![Dir::Left, Dir::Right],
+                Dir::Left => vec![Dir::Up, Dir::Down],
+                Dir::Right => vec![Dir::Up, Dir::Down],
+            };
+
+            let dist = 0;
+            return possible_dirs
+                .into_iter()
+                .map(|dir| match dir {
+                    Dir::Up => Pos(x, y - 1, dir, dist + 1),
+                    Dir::Down => Pos(x, y + 1, dir, dist + 1),
+                    Dir::Left => Pos(x - 1, y, dir, dist + 1),
+                    Dir::Right => Pos(x + 1, y, dir, dist + 1),
+                })
+                .map(|p| (p, 1))
+                .collect();
+        } else {
+            let possible_dirs = match dir {
+                Dir::Up => vec![Dir::Up, Dir::Left, Dir::Right],
+                Dir::Down => vec![Dir::Down, Dir::Left, Dir::Right],
+                Dir::Left => vec![Dir::Left, Dir::Up, Dir::Down],
+                Dir::Right => vec![Dir::Right, Dir::Up, Dir::Down],
+            };
+
+            return possible_dirs
+                .into_iter()
+                .map(|dir| match dir {
+                    Dir::Up => Pos(x, y - 1, dir, dist + 1),
+                    Dir::Down => Pos(x, y + 1, dir, dist + 1),
+                    Dir::Left => Pos(x - 1, y, dir, dist + 1),
+                    Dir::Right => Pos(x + 1, y, dir, dist + 1),
+                })
+                .map(|p| (p, 1))
+                .collect();
+        };
+    }
+}
+
+// assert_eq!(result.expect("no path found").1, 4);
+
 fn main() {
     let input = std::fs::read_to_string("src/input/d17p1.txt").unwrap();
 
@@ -7,78 +86,40 @@ fn main() {
         .map(|line| line.trim().chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
-    let loss = least_loss_path(&grid, (0, 0), 0, 0, Dir::Right);
+    let goal = (grid[0].len() as i32 - 1, grid.len() as i32 - 1);
 
-    println!("{}", loss);
+    let start_weight = Pos(0, 0, Dir::Right, 0).loss(&grid);
+
+    let result = astar(
+        &Pos(0, 0, Dir::Right, 0),
+        |p| p.successors(),
+        |p| p.distance(goal) + p.loss(&grid) * 3,
+        |p| p.0 == goal.0 && p.1 == goal.1,
+    )
+    .unwrap();
+
+    let l = result.0.iter().fold(0, |acc, p| acc + p.loss(&grid));
+
+    print_grid(&grid, &result);
+    println!();
+    println!("Result: {}", l - start_weight);
 }
 
-// recursively find the path with the least loss
-// you incur loss when you move to a new cell (cell is number of loss
-// complete when you reach bottom right corner
-// only can go straight or rotate 90 degrees at a time
-// once traveling 3 cells in a direction, you must turn
-fn least_loss_path(
-    grid: &Vec<Vec<char>>,
-    pos: (isize, isize),
-    loss: usize,
-    dist: usize,
-    dir: Dir,
-) -> usize {
-    if pos == (grid[0].len() as isize - 1, grid.len() as isize - 1) {
-        return loss;
+fn print_grid(grid: &Vec<Vec<char>>, result: &(Vec<Pos>, u32)) {
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            let mut found = false;
+            for p in &result.0 {
+                if p.0 == x as i32 && p.1 == y as i32 {
+                    print!("{} ", p.loss(&grid));
+                    found = true;
+                    break;
+                }
+            }
+            if !found {
+                print!("  ");
+            }
+        }
+        println!();
     }
-
-    let possible_dirs = if dist == 3 {
-        match dir {
-            Dir::Up => vec![Dir::Left, Dir::Right],
-            Dir::Down => vec![Dir::Left, Dir::Right],
-            Dir::Left => vec![Dir::Up, Dir::Down],
-            Dir::Right => vec![Dir::Up, Dir::Down],
-        }
-    } else {
-        match dir {
-            Dir::Up => vec![Dir::Up, Dir::Left, Dir::Right],
-            Dir::Down => vec![Dir::Down, Dir::Left, Dir::Right],
-            Dir::Left => vec![Dir::Left, Dir::Up, Dir::Down],
-            Dir::Right => vec![Dir::Right, Dir::Up, Dir::Down],
-        }
-    };
-
-    for dir in possible_dirs {
-        let mut loss = loss;
-        let mut dist = dist;
-        let mut pos = pos;
-
-        let char = get_char(&grid, pos);
-
-        if let Some(char) = char {
-            loss += char.to_digit(10).unwrap() as usize;
-        }
-
-        dist += 1;
-
-        match dir {
-            Dir::Up => pos.1 -= 1,
-            Dir::Down => pos.1 += 1,
-            Dir::Left => pos.0 -= 1,
-            Dir::Right => pos.0 += 1,
-        }
-
-        least_loss_path(grid, pos, loss, dist, dir);
-    }
-
-    0
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Dir {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-fn get_char(grid: &Vec<Vec<char>>, pos: (isize, isize)) -> Option<char> {
-    let c = grid.get(pos.1 as usize)?.get(pos.0 as usize)?;
-    Some(*c)
 }
