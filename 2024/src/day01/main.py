bae21b0a4f07af61390af62fdbb541392ee8bb1e
westy12dan <westy12dan@gmail.com>
2024-12-02 09:30:09 +0800

    day1

--- 2024/src/day01/main.py
+++ 2024/src/day01/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-def main(name: str):
+def main_p1(name: str):
     file = open(Path(__file__).parent / name, "r").read()
     lines = file.strip().split("\n")
 
@@ -29,9 +29,42 @@ def main(name: str):
     return total
 
 
+def main_p2(name: str):
+    file = open(Path(__file__).parent / name, "r").read()
+    lines = file.strip().split("\n")
+
+    aArr: list[int] = []
+    bArr: list[int] = []
+
+    for line in lines:
+        a, b = line.split("  ")
+        aArr.append(int(a))
+        bArr.append(int(b))
+
+    total = 0
+
+    for i in range(len(aArr)):
+        a = aArr[i]
+
+        found = 0
+
+        for j in range(len(bArr)):
+            b = bArr[j]
+            if b == a:
+                found += 1
+
+        total += found * a
+
+    return total
+
+
 def test_sample():
-    assert main("sample.txt") == 11
+    assert main_p1("sample.txt") == 11
+
+
+def test_p1():
+    assert main_p1("p1.txt") == 1660292
 
 
 if __name__ == "__main__":
-    print(main("p1.txt"))
+    print(main_p2("p1.txt"))
