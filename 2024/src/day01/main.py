d94414958b9cbb42ad45bfce06ebd4a96906ed09
westy12dan <westy12dan@gmail.com>
2024-12-02 09:59:51 +0800

    improve

--- 2024/src/day01/main.py
+++ 2024/src/day01/main.py
@@ -1,15 +1,15 @@
+from collections import Counter
 from pathlib import Path
 
 
 def main_p1(name: str):
-    file = open(Path(__file__).parent / name, "r").read()
-    lines = file.strip().split("\n")
+    lines = open(Path(__file__).parent / name, "r").readlines()
 
     aArr: list[int] = []
     bArr: list[int] = []
 
     for line in lines:
-        a, b = line.split("  ")
+        a, b = line.split()
         aArr.append(int(a))
         bArr.append(int(b))
 
@@ -21,39 +21,28 @@ def main_p1(name: str):
     for i in range(len(bArr)):
         a = aArr[i]
         b = bArr[i]
-        if b > a:
-            total += b - a
-        else:
-            total += a - b
+        total += abs(b - a)
 
     return total
 
 
 def main_p2(name: str):
-    file = open(Path(__file__).parent / name, "r").read()
-    lines = file.strip().split("\n")
+    lines = open(Path(__file__).parent / name, "r").readlines()
 
     aArr: list[int] = []
     bArr: list[int] = []
 
     for line in lines:
-        a, b = line.split("  ")
+        a, b = line.split()
         aArr.append(int(a))
         bArr.append(int(b))
 
     total = 0
+    cnt = Counter(bArr)
 
     for i in range(len(aArr)):
         a = aArr[i]
-
-        found = 0
-
-        for j in range(len(bArr)):
-            b = bArr[j]
-            if b == a:
-                found += 1
-
-        total += found * a
+        total += cnt[a] * a
 
     return total
 
@@ -66,5 +55,9 @@ def test_p1():
     assert main_p1("p1.txt") == 1660292
 
 
+def test_p2():
+    assert main_p2("p1.txt") == 22776016
+
+
 if __name__ == "__main__":
-    print(main_p2("p1.txt"))
+    print(main_p1("sample.txt"))
