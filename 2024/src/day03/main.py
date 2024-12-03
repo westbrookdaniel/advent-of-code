6be3df994384e55f4e8ed57b5f0aabb489ea92aa
westy12dan <westy12dan@gmail.com>
2024-12-03 13:46:23 +0800

    refactor

--- 2024/src/day03/main.py
+++ 2024/src/day03/main.py
@@ -5,15 +5,13 @@ from pathlib import Path
 def part1(name: str):
     input = open(Path(__file__).parent / name, "r").read()
 
-    matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", input)
+    matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", input)
 
     total = 0
 
     for match in matches:
-        left, right = match.split(",")
-        left = int(left[4:])
-        right = int(right[:-1])
-        total += left * right
+        left, right = match
+        total += int(left) * int(right)
 
     return total
 
@@ -21,26 +19,19 @@ def part1(name: str):
 def part2(name: str):
     input = open(Path(__file__).parent / name, "r").read()
 
-    matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", input)
+    matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))", input)
 
     total = 0
     enabled = True
 
     for match in matches:
-        if match == "do()":
+        left, right, do, dont = match
+        if do:
             enabled = True
-            continue
-        if match == "don't()":
+        elif dont:
             enabled = False
-            continue
-
-        if not enabled:
-            continue
-
-        left, right = match.split(",")
-        left = int(left[4:])
-        right = int(right[:-1])
-        total += left * right
+        elif enabled:
+            total += int(left) * int(right)
 
     return total
 
