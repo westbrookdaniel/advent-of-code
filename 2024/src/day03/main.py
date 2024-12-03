3dd7d4acb1474e5f51ee4595591c9ae4c0bcf310
westy12dan <westy12dan@gmail.com>
2024-12-03 13:19:17 +0800

    done day3 original

---
+++ 2024/src/day03/main.py
@@ -0,0 +1,66 @@
+import re
+from pathlib import Path
+
+
+def part1(name: str):
+    input = open(Path(__file__).parent / name, "r").read()
+
+    matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", input)
+
+    total = 0
+
+    for match in matches:
+        left, right = match.split(",")
+        left = int(left[4:])
+        right = int(right[:-1])
+        total += left * right
+
+    return total
+
+
+def part2(name: str):
+    input = open(Path(__file__).parent / name, "r").read()
+
+    matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", input)
+
+    total = 0
+    enabled = True
+
+    for match in matches:
+        if match == "do()":
+            enabled = True
+            continue
+        if match == "don't()":
+            enabled = False
+            continue
+
+        if not enabled:
+            continue
+
+        left, right = match.split(",")
+        left = int(left[4:])
+        right = int(right[:-1])
+        total += left * right
+
+    return total
+
+
+def test_sample_p1():
+    assert part1("sample.txt") == 161
+
+
+# def test_p1():
+#     assert part1("input.txt") == 559
+
+
+# def test_sample_p2():
+#     assert part2("sample.txt") == 4
+
+
+# def test_p2():
+#     assert part2("input.txt") == 601
+
+
+if __name__ == "__main__":
+    print("")
+    print(part2("input.txt"))
