0d549c5baf586640861fdf5ab24ee3698dadbd94
Daniel Westbrook <westy12dan@gmail.com>
2025-12-01 13:59:33 +0800

    part2

---
+++ 2025/src/day01/part2.ts
@@ -0,0 +1,28 @@
+import assert from "assert";
+import fs from "fs";
+
+const input = fs.readFileSync("2025/src/day01/input.txt", "utf8");
+const lines = input.trim().split("\n");
+
+let pos = 50;
+let total = 0;
+
+for (const line of lines) {
+  const [dir, ...str] = line.split("");
+  const n = parseInt(str.join(""));
+
+  for (let i = 0; i < n; i++) {
+    if (dir === "L") pos -= 1;
+    else pos += 1;
+
+    if (pos < 0) pos = 99;
+    if (pos > 99) pos = 0;
+
+    if (pos === 0) {
+      total += 1;
+    }
+  }
+}
+
+console.log();
+console.log(total);
