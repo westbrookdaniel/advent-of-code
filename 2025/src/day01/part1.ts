e2befcaff230a17a9a71cee20e25222e511d27d8
Daniel Westbrook <westy12dan@gmail.com>
2025-12-01 13:14:39 +0800

    p1

--- 2025/src/day01/part1.ts
+++ 2025/src/day01/part1.ts
@@ -3,3 +3,31 @@ import fs from "fs";
 
 const input = fs.readFileSync(import.meta.dir + "/input.txt", "utf8");
 const lines = input.trim().split("\n");
+
+let pos = 50;
+let total = 0;
+
+for (const line of lines) {
+  const [dir, ...str] = line.split("");
+  const n = parseInt(str.join(""));
+
+  if (dir === "L") {
+    pos -= n;
+  } else {
+    pos += n;
+  }
+
+  while (pos < 0 || pos > 99) {
+    if (pos < 0) {
+      pos = 100 + pos;
+    } else if (pos > 99) {
+      pos = pos - 100;
+    }
+  }
+
+  if (pos === 0) {
+    total += 1;
+  }
+}
+
+console.log(total);
