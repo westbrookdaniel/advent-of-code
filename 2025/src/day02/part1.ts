5fdc6be7452e54df61a6ed886c3f39df8668a2a4
Daniel Westbrook <westy12dan@gmail.com>
2025-12-02 13:13:55 +0800

    checkpoint

--- 2025/src/day02/part1.ts
+++ 2025/src/day02/part1.ts
@@ -2,6 +2,30 @@ import assert from "assert";
 import fs from "fs";
 
 const input = fs.readFileSync(import.meta.dir + "/input.txt", "utf8");
-const lines = input.trim().split("\n");
+const ranges = input.trim().split(",");
 
-console.log(lines);
+const invalidIds = [];
+
+for (const range of ranges) {
+  const [start, end] = range.split("-").map((n) => parseInt(n));
+
+  if (!start) assert.fail("Not valid start");
+  if (!end) assert.fail("Not valid end");
+
+  for (let id = start; id <= end; id++) {
+    const idStr = id.toString();
+
+    const halfLength = idStr.length / 2;
+    if (halfLength !== Math.floor(halfLength)) continue;
+
+    const first = idStr.slice(0, halfLength);
+    const second = idStr.slice(halfLength);
+    if (first !== second) continue;
+
+    invalidIds.push(id);
+  }
+}
+
+const total = invalidIds.reduce((a, b) => a + b);
+
+console.log(total);
