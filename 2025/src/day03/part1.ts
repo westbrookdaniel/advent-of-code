fdce3ea488d99201ee48aa87bbe004a422cf3ba6
Daniel Westbrook <westy12dan@gmail.com>
2025-12-03 13:41:12 +0800

    checkpoint

--- 2025/src/day03/part1.ts
+++ 2025/src/day03/part1.ts
@@ -2,5 +2,38 @@ import assert from "assert";
 import fs from "fs";
 
 const input = fs.readFileSync(import.meta.dir + "/input.txt", "utf8");
-const lines = input.trim().split("\n");
+const banks = input.trim().split("\n");
 
+let total = 0;
+
+for (const bank of banks) {
+  const batteries = bank.split("").map((n) => parseInt(n));
+
+  const largestIndex = batteries.reduce((largestIndex, bat, i, arr) => {
+    const largest = arr[largestIndex];
+    if (largest === undefined) assert.fail("oob");
+
+    if (i === arr.length - 1) return largestIndex;
+    if (bat > largest) return i;
+    return largestIndex;
+  }, 0);
+
+  const second = batteries.slice(largestIndex + 1);
+
+  const largestSecondIndex = second.reduce((largestIndex, bat, i, arr) => {
+    const largest = arr[largestIndex];
+    if (largest === undefined) assert.fail("oob");
+
+    if (bat > largest) return i;
+    return largestIndex;
+  }, 0);
+
+  const f = batteries[largestIndex]?.toString() ?? "";
+  const s = second[largestSecondIndex]?.toString() ?? "";
+
+  const joltage = parseInt(f + s);
+
+  total += joltage;
+}
+
+console.log(total);
