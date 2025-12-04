355dfd02a0bf2ca3d102ce60674f0ee1f39fcf75
Daniel Westbrook <westy12dan@gmail.com>
2025-12-04 13:09:36 +0800

    checkpoint

---
+++ 2025/src/day04/part1.ts
@@ -0,0 +1,7 @@
+import assert from "assert";
+import fs from "fs";
+
+const input = fs.readFileSync(import.meta.dir + "/input.txt", "utf8");
+const lines = input.trim().split("\n");
+
+console.log(lines);
