0e3cb5dd05a4ddd89ab9b82d9e9876904d16a1bf
Daniel Westbrook <westy12dan@gmail.com>
2025-12-08 09:24:07 +0800

    checkpoint

--- 2025/src/day06/part1.ts
+++ 2025/src/day06/part1.ts
@@ -1,5 +1,44 @@
 import assert from "assert";
 import fs from "fs";
+import { zip } from "es-toolkit";
 
 const input = fs.readFileSync(import.meta.dir + "/input.txt", "utf8");
-const lines = input.trim().split("\n");
+
+const grid = input
+  .trim()
+  .split("\n")
+  .map((line) => {
+    const strs: string[] = [];
+    const parts = line.trim().split(/\s+/);
+    for (const part of parts) {
+      if (part.length > 0) strs.push(part);
+    }
+    return strs;
+  });
+
+const cols = zip(...grid);
+
+let total = 0;
+
+for (const col of cols) {
+  const method = col.pop();
+  const numbers = col.map((n) => parseInt(n));
+
+  let sum = 0;
+
+  if (method === "+") {
+    sum = numbers.reduce((acc, n) => {
+      return acc + n;
+    });
+  }
+
+  if (method === "*") {
+    sum = numbers.reduce((acc, n) => {
+      return acc * n;
+    });
+  }
+
+  total += sum;
+}
+
+console.log(total);
